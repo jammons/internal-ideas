6e3ec0a27ad2229b3d76e5f143c140af58f1795a
Jeff Ammons <jammons@mahalo.com>
2010-12-03 18:00:08 -0800

    Hopefully fixing this settings issue.

--- settings/__init__.py
+++ settings/__init__.py
@@ -3,7 +3,7 @@ import platform
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 
-if PROJECT_PATH.startswith('/var/www/ideas/'): #PRODUCTION
+if PROJECT_PATH.startswith('/var/www/ideas'): #PRODUCTION
     from prod import *
 else:
     from local import *
